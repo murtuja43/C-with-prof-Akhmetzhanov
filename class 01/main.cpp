771e975dc45ea99c9e98a16722bfd75774f1c064
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2025-10-03 19:22:13 +0500

    added the first class of the course

---
+++ class 01/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  class 01
+//
+//  Created by Md Golam Murtuja Kayes on 10/3/25.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    cout<<"Hello World"<<endl;
+    return 0;
+}
