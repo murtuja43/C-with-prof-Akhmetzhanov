c2c90e3f76062a3fe0293c7564e7f4ecef8045f2
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2025-10-04 13:58:51 +0500

    Added class 2

---
+++ Class 2/main.cpp
@@ -0,0 +1,142 @@
+//
+//  main.cpp
+//  Class 2
+//
+//  Created by Md Golam Murtuja Kayes on 10/4/25.
+//
+
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+
+{
+    cout<<"Hello prof Akhmetzhanov";
+    cout<<"Hello world"<<endl;
+    cout<<"New line Hello world"<<endl<<endl;
+
+    
+//    problem 1: Begin1 (from book)
+    /*
+    int a, P, Pi;
+    a = 5;
+    Pi = 3.14;
+    cout<<"a= ";
+    cin>>a;
+    P = 4 * a;
+    cout<<"P= "<<P<<endl;
+     return 0;
+     */
+    
+    
+    
+//    prolem 2: Begin2 (from book)
+    /*
+    int a, S;
+    cout<<"a = ";
+    cin>>a;
+    S = pow(a, 2);
+    cout<<"S= "<<S<<endl;
+     return 0;
+     */
+    
+    
+//    problem 3: Begin3 (from book)
+    /*
+    int a, b, S, P;
+    cout<<"a= ";
+    cin>>a;
+    cout<<"b= ";
+    cin>>b;
+    
+    S = a * b;
+    P = 2*(a+b);
+    cout<<"S= "<<S<<endl;
+    cout<<"P= "<<P<<endl;
+    return 0;
+     */
+    
+    
+//problem 4: Begin4 (from book)
+    /*
+    int d, pi, L;
+    cout<<"d= ";
+    cin>>d;
+    pi = 3.14;
+    L = pi * d;
+    cout<<"L= "<<L<<endl;
+    return 0;
+     */
+    
+    
+//problem 5: Begin5 (from book)
+    /*
+    int a, V, S;
+    cout<<"a= ";
+    cin>>a;
+    V = pow(a,3);
+    S = 6*(pow(a,2));
+    cout<<"V= "<<V<<endl;
+    cout<<"S= "<<S<<endl;
+    return 0;
+     */
+    
+
+//problem 6: Begin6 (from book)
+    /*
+    float a, b, c, V, S;
+    cout<<"a= ";
+    cin>>a;
+    cout<<"b= ";
+    cin>>b;
+    cout<<"c= ";
+    cin>>c;
+    
+    V = (a * b * c);
+    S = 2*((a*b)+(b*c)+(a*c));
+    
+    cout<<"V= "<<V<<endl;
+    cout<<"S= "<<S<<endl;
+    return 0;
+    */
+    
+    
+//prbolem 7: Begin7 (from book)
+    /*
+    float R, L, S, pi;
+    cout<<"R= ";
+    cin>>R;
+    pi = 3.14;
+    
+    L = 2 * pi * R;
+    S = pi * (pow(R, 2));
+    
+    cout<<"L= "<<L<<endl;
+    cout<<"R= "<<S<<endl;
+    return 0;
+    */
+    
+
+//prbolem 8: Begin8 (from book)
+    /*
+    float a, b, arithmean; //we can use "double" insted of float too
+    cout<<"a= ";
+    cin>>a;
+    cout<<"b= ";
+    cin>>b;
+    
+    arithmean = (a+b)/2;
+    cout<<"Arithmetic mean= "<<arithmean<<endl;
+    return 0;
+    */
+
+    
+//prbolem 9: Begin9 (from book)
+//    s=sqrt(a+2)
+    
+    
+    
+}
