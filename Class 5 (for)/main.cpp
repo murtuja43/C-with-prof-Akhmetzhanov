6054586c6746fb71fb31d3f14cea32892f1834d7
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2025-11-01 13:47:13 +0500

    Add files via upload

---
+++ Class 5 (for)/main.cpp
@@ -0,0 +1,124 @@
+//
+//  main.cpp
+//  Class 5 (for)
+//
+//  Created by Md Golam Murtuja Kayes on 11/1/25.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//for1
+
+//int main() {
+//    int K, N;
+//    cin >> K >> N;
+//    for (int i = 0; i < N; i++)
+//        cout << "K= " << K << " ";
+//    return 0;
+//}
+
+
+//for2
+//int main() {
+//    int A, B;
+//    cin >> A >> B;
+//    for (int i = A; i <= B; i++)
+//        cout << i << " ";
+//    cout << endl << "N = " << (B - A + 1);
+//    return 0;
+//}
+
+
+//for3
+//int main() {
+//    int A, B;
+//    cin >> A >> B;
+//    for (int i = B - 1; i > A; i--)
+//        cout << i << " ";
+//    cout << endl << "N = " << (B - A - 1);
+//    return 0;
+//}
+//
+
+
+//while
+//example1
+//int main(){
+//    int i;
+//    i = 0;
+//    while (i <10)
+//    {
+//        i++;
+//    }
+//    cout<<i;
+//}
+
+
+
+//example2
+//do
+//{
+//    i++
+//    ....
+//}
+//while (i<N);
+//cout<<i; i=10;
+
+
+
+//While1
+//
+//int main () {
+//    double A, B;
+//    cout <<"Enter A and B: ";
+//    cin >> A >> B;
+//    
+//    while (A >= B) {
+//        A = A - B;
+//    }
+//    
+//    cout << "Unused part = " << A << endl;
+//    return 0;
+//}
+
+
+//While2
+//int main () {
+//    double A, B, i;
+//    cout << "Enter A and B: ";
+//    cin >> A >> B;
+//    
+//    i = 0;
+//    while (A >= B) {
+//        A = A - B;
+//        i++;
+//    }
+//    cout << i << endl;
+//}
+
+
+//While3
+//int main() {
+//    int N, K, quotient;
+//    cout << "Enter N and K: ";
+//    cin >> N >> K;
+//    
+//    quotient = 0;
+//    while (N >= K) {
+//        N = N - K;
+//        quotient++;
+//    }
+//    cout << "Quotent = " << quotient << endl;
+//    cout << "Remainder = " << N << endl;
+//}
+
+
+
+
+//
+//
+//Hometask-
+//1. for 36-40 (5 questions)
+//2. while 1-30 step 3 (10 questions)
